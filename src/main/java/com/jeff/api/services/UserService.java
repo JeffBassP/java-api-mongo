2389580d72a02be156ef35b361c3468f3baef74f
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-09-23 16:16:55 -0300

    metodo post

--- src/main/java/com/jeff/api/services/UserService.java
+++ src/main/java/com/jeff/api/services/UserService.java
@@ -2,11 +2,13 @@ package com.jeff.api.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.ObjDoubleConsumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jeff.api.domain.User;
+import com.jeff.api.dto.UserDTO;
 import com.jeff.api.repository.UserRepository;
 import com.jeff.api.services.exception.ObjectNotFoundException;
 
@@ -25,5 +27,13 @@ public class UserService {
 		Optional<User> user = userRepository.findById(id);
 
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 }
