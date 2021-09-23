7e91dab561f2a089a776bd0ee952af45617c03bf
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-09-23 15:33:24 -0300

    create FindById and ExceptionHandler

--- src/main/java/com/jeff/api/services/UserService.java
+++ src/main/java/com/jeff/api/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jeff.api.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jeff.api.domain.User;
 import com.jeff.api.repository.UserRepository;
+import com.jeff.api.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,11 @@ public class UserService {
 	public List<User> findAll() {
 
 		return userRepository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
