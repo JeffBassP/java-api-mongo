52d8e43bf5bd45de0a0c5a8f2a28d4c2d0ead34e
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-09-23 19:41:15 -0300

    modelagem dos dados do author com DTO

--- src/main/java/com/jeff/api/config/Instantiation.java
+++ src/main/java/com/jeff/api/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.jeff.api.domain.Post;
 import com.jeff.api.domain.User;
+import com.jeff.api.dto.AuthorDTO;
 import com.jeff.api.repository.PostRepository;
 import com.jeff.api.repository.UserRepository;
 
@@ -18,30 +19,32 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2021"),"Partiu Viagem", "Viagem para NY, muito legal!!", maria );
-		Post post2 = new Post(null, sdf.parse("23/03/2021"),"Bom dia", "Um excelente dia a todos.", maria );
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2021"), "Partiu Viagem", "Viagem para NY, muito legal!!",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2021"), "Bom dia", "Um excelente dia a todos.",
+				new AuthorDTO(maria));
+
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		
-		
-		
+
 	}
 
 }
