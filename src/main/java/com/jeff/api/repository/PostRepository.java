a36558103d95241551debdf14eaf8fd5dbd0b8ef
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-09-24 16:28:35 -0300

    Finalizando api com mongo - consulta com varios criterios

--- src/main/java/com/jeff/api/repository/PostRepository.java
+++ src/main/java/com/jeff/api/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.jeff.api.repository;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -16,4 +17,8 @@ public interface PostRepository extends MongoRepository<Post, Serializable> {
 	
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchByTitle(String text);
+	
+	@Query("{ $and: [ {date: {$gte: ?1} }, { date: { $lte: ?2}  }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] }\r\n"
+			+ " ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
